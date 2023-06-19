40611f3797be09948ecb1979853cf79b4a221c72
Yanming Sun <tinasun518@gmail.com>
2023-06-19 07:46:19 -0500

    add process in letters

--- LetterService/Program.cs
+++ LetterService/Program.cs
@@ -1,46 +1,108 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
+using System.IO;
+using System.Linq;
 
-using System.Xml.Serialization;
-
-public interface ILetterService 
+namespace ConsoleApp
 {
-    void CombineTwoLetters(string inputFile1, string inputFile2, string resultFile);
+    interface ILetterService
+    {
+        void CombineTwoLetters(string inputFile1, string inputFile2, string resultFile);
+    }
 
+    class LetterService : ILetterService
+    {
+        public void CombineTwoLetters(string inputFile1, string inputFile2, string resultFile)
+        {
+            // Combine the contents of inputFile1 and inputFile2 into resultFile
+            string content1 = File.ReadAllText(inputFile1);
+            string content2 = File.ReadAllText(inputFile2);
+            string combinedContent = content1 + Environment.NewLine + content2;
+            File.WriteAllText(resultFile, combinedContent);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ...
+            // Define folder paths and other variables
+
+            // Create an instance of the LetterService
+            ILetterService letterService = new LetterService();
+
+            string rootFolder = "C:\\Users\\kelma\\source\\repos\\CombinedLetters";
+            string inputFolder = Path.Combine(rootFolder, "Input");
+            string archiveFolder = Path.Combine(rootFolder, "Archive");
+            string outputFolder = Path.Combine(rootFolder, "Output");
+
+            // Create dated folder path based on the current date
+            string currentDateFolder = Path.Combine(inputFolder, ""); // DateTime.Today.ToString("yyyyMMdd")
+
+
+
+            // Call the ProcessLetters method
+            ProcessLetters(currentDateFolder, outputFolder, letterService);
+
+            // ...
+            // Rest of the code
+        }
+
+        static void ProcessLetters(string sourceFolder, string outputFolder, ILetterService letterService)
+        {
+            string admissionFolder = Path.Combine(sourceFolder, "Admission");
+            string scholarshipFolder = Path.Combine(sourceFolder, "Scholarship");
+            string processDate = "20220125";
+            // Process admission letters
+            ProcessLettersInFolder(admissionFolder, outputFolder, processDate, letterService);
+
+            // Process scholarship letters
+            ProcessLettersInFolder(scholarshipFolder, outputFolder, processDate, letterService);
+        }
+
+        static void ProcessLettersInFolder(string folderPath, string outputFolder, string targetDate, ILetterService letterService)
+        {
+            if (Directory.Exists(folderPath))
+            {
+                string targetFolderPath = Path.Combine(folderPath, targetDate);
+
+                if (Directory.Exists(targetFolderPath))
+                {
+                    string[] letterFiles = Directory.GetFiles(targetFolderPath, "*.txt");
+
+                    foreach (string letterFile in letterFiles)
+                    {
+                        string universityID = Path.GetFileNameWithoutExtension(letterFile).Substring(10);
+                        string combinedLetterPath = Path.Combine(outputFolder, $"combined-{universityID}.txt");
+
+                        // Check if any existing combined letter for the same university ID exists
+                        if (File.Exists(combinedLetterPath))
+                        {
+                            // Read the existing combined letter content
+                            string existingCombinedContent = File.ReadAllText(combinedLetterPath);
+
+                            // Read the current letter content
+                            string currentLetterContent = File.ReadAllText(letterFile);
+
+                            // Combine the current letter content with the existing combined letter content
+                            string combinedContent = existingCombinedContent + Environment.NewLine + currentLetterContent;
+
+                            // Update the combined letter file with the new content
+                            File.WriteAllText(combinedLetterPath, combinedContent);
+                        }
+                        else
+                        {
+                            // No existing combined letter, create a new one
+                            letterService.CombineTwoLetters(letterFile, string.Empty, combinedLetterPath);
+                        }
+
+                    }
+
+                }
+            }
+        }
+    }
+
+    // ...
+    // Rest of the code
 }
-
-public class LetterService : ILetterService 
-{
-
-    public void CombineTwoLetters(string inputFile1, string inputFile2, string resultFile) 
-    {
-        string filePath = "path/to/your/newfile.txt";
-        string fileContents = "This is the content to write into the file.";
-        File.WriteAllText(filePath, fileContents);
-    }
-
-
-
-    public static string FindCombinedLetters(string studentId) 
-    {
-        string filePath = string.Format("../{0}");
-        File.CreateText(filePath).Close();
-        File.WriteAllText(filePath, "test");
-        return filePath;
-    }
-
-
-
-     static void Main(string[] args)
-    {
-        Console.WriteLine("Enter your name:");
-        string name = Console.ReadLine();
-        string test = FindCombinedLetters(DateTime.Now.ToString("yyyyMMdd"));
-        Console.WriteLine($"Hello, {name}! Welcome to your console app.");
-        Console.ReadLine();
-    }
-}
-
-
-
-
-
