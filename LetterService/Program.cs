c6445136d52effebd55c5bee6b1cf63d58a85bf8
Yanming Sun <tinasun518@gmail.com>
2023-06-18 23:32:11 -0500

    Initial commit

---
+++ LetterService/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+public interface ILetterService 
+{
+    void CombineTwoLetters(string inputFile1, string inputFile2, string resultFile);
+
+}
+
+public class LetterService : ILetterService 
+{
+
+    public void CombineTwoLetters(string inputFile1, string inputFile2, string resultFile) 
+    {
+        Console.WriteLine("Hello, World!");
+
+    }
+
+
+}
+
+
+
