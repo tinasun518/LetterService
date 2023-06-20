59587a9605f560f628a2ec322b37367a1830fe0b
Yanming Sun <tinasun518@gmail.com>
2023-06-19 20:29:47 -0500

    working version

--- LetterService/Program.cs
+++ LetterService/Program.cs
@@ -2,7 +2,7 @@
 using System.IO;
 using System.Linq;
 
-namespace ConsoleApp
+namespace LetterService
 {
     interface ILetterService
     {
@@ -13,15 +13,8 @@ namespace ConsoleApp
     {
         public void CombineTwoLetters(string inputFile1, string inputFile2, string resultFile)
         {
-            // Combine the contents of inputFile1 and inputFile2 into resultFile
-            string content1 = File.ReadAllText(inputFile1);
-            string content2 = string.Empty;
-
-            if (!string.IsNullOrEmpty(inputFile2))
-            {
-                content2 = File.ReadAllText(inputFile2);
-            }
-
+            string content1 = string.IsNullOrEmpty(inputFile1) ? "" : File.ReadAllText(inputFile1);
+            string content2 = string.IsNullOrEmpty(inputFile2) ? "" : File.ReadAllText(inputFile2);
             string combinedContent = content1 + Environment.NewLine + content2;
             File.WriteAllText(resultFile, combinedContent);
         }
@@ -29,97 +22,163 @@ namespace ConsoleApp
 
     class Program
     {
+        //Set default values
+        static string rootFolder = "CombinedLetters";
+        static string inputFolder = Path.Combine(rootFolder, "Input");
+        static string archiveFolder = Path.Combine(rootFolder, "Archive");
+        static string outputFolder = Path.Combine(rootFolder, "Output");
+        static string processDate = "20220125"; //DateTime.Now.ToString("yyyyMMdd");
+
         static void Main(string[] args)
         {
-            // ...
-            // Define folder paths and other variables
-
-            // Create an instance of the LetterService
+            if (!ParseParameters(args))
+            {
+                Console.WriteLine("Some parameters are invalid, please check and retry.");
+                PrintUsage();
+            }
             ILetterService letterService = new LetterService();
 
-            string rootFolder = "C:\\Users\\kelma\\source\\repos\\LetterService\\CombinedLetters";
-            string inputFolder = Path.Combine(rootFolder, "Input");
-            string archiveFolder = Path.Combine(rootFolder, "Archive");
-            string outputFolder = Path.Combine(rootFolder, "Output");
-
-            // Create dated folder path based on the current date
-            string currentDateFolder = Path.Combine(inputFolder, ""); // DateTime.Today.ToString("yyyyMMdd")
-
-
-
-            // Call the ProcessLetters method
-            ProcessLetters(currentDateFolder, outputFolder, letterService);
-
-            // ...
-            // Rest of the code
-        }
-
-        static void ProcessLetters(string sourceFolder, string outputFolder, ILetterService letterService)
-        {
-            string admissionFolder = Path.Combine(sourceFolder, "Admission");
-            string scholarshipFolder = Path.Combine(sourceFolder, "Scholarship");
-            string processDate = "20220125";
-            // Process admission letters
-            ProcessLettersInFolder(admissionFolder, outputFolder, processDate, letterService);
-
-            // Process scholarship letters
-            ProcessLettersInFolder(scholarshipFolder, outputFolder, processDate, letterService);
-        }
-
-        static void ProcessLettersInFolder(string folderPath, string outputFolder, string targetDate, ILetterService letterService)
-        {
-            if (Directory.Exists(folderPath))
+            //Start execution.
+            try
             {
-                string targetFolderPath = Path.Combine(folderPath, targetDate);
+                Console.WriteLine("Start Execution.");
+                Excute(inputFolder, outputFolder, letterService, processDate, archiveFolder);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Encountered error: " + ex.Message);
+                Console.WriteLine("Stack: " + ex.StackTrace);
+            }
+            finally
+            {
+                Console.WriteLine("Finished Execution.");
+            }
+        }
 
-                if (Directory.Exists(targetFolderPath))
+        static bool ParseParameters(string[] args)
+        {
+            if (args.Length % 2 != 0)
+            {
+                Console.WriteLine("Invalid number of parameters.");
+                return false;
+            }
+
+            for (int i = 0; i < args.Length; i += 2)
+            {
+                string option = args[i].ToLower();
+                string value = args[i + 1];
+
+                switch (option)
                 {
-                    var combinedLetters = new Dictionary<string, List<string>>();
-
-                    string[] letterFiles = Directory.GetFiles(targetFolderPath, "*.txt");
-
-                    foreach (string letterFile in letterFiles)
-                    {
-                        string universityID = Path.GetFileNameWithoutExtension(letterFile).Substring(10);
-
-                        if (!combinedLetters.ContainsKey(universityID))
+                    case "-i":
+                        if (!Directory.Exists(value)) { return false; }
+                        inputFolder = value;
+                        break;
+                    case "-o":
+                        if (!Directory.Exists(value)) { return false; }
+                        outputFolder = value;
+                        break;
+                    case "-d":
+                        if (!DateTime.TryParseExact(value, "yyyyMMdd", null, System.Globalization.DateTimeStyles.None, out _))
                         {
-                            combinedLetters[universityID] = new List<string>();
+                            Console.WriteLine("Invalid date format. Expected format: yyyyMMdd");
+                            return false;
                         }
-
-                        combinedLetters[universityID].Add(letterFile);
-                    }
-
-                    foreach (var kvp in combinedLetters)
-                    {
-                        string universityID = kvp.Key;
-                        List<string> letterFilesForUniversity = kvp.Value;
-
-                        string combinedLetterPath = Path.Combine(outputFolder, $"combined-{universityID}.txt");
-
-                        if (letterFilesForUniversity.Count == 1)
-                        {
-                            // Only one letter file, no need to combine
-                            File.Copy(letterFilesForUniversity[0], combinedLetterPath);
-                        }
-                        else
-                        {
-                            // Combine multiple letter files
-                            CombineMultipleLetters(letterFilesForUniversity, combinedLetterPath, letterService);
-                        }
-                    }
+                        break;
+                    case "-r":
+                        if (!Directory.Exists(value)) { return false; }
+                        rootFolder = value;
+                        break;
+                    case "-a":
+                        if (!Directory.Exists(value)) { return false; }
+                        archiveFolder = value;
+                        break;
+                    default:
+                        return false;
                 }
             }
+            return true;
+        }
+
+        static void PrintUsage()
+        {
+            Console.WriteLine("Usage: LetterService.exe -i <input path> -o <output path> -d <date> -r <root folder path> -a <archive path>");
+            Console.WriteLine("-i <input path>      : Path to the input folder, will overwrite root folder's input path.");
+            Console.WriteLine("-o <output path>     : Path to the output folder, will overwrite root folder's output path.");
+            Console.WriteLine("-d <date>            : Date in yyyyMMdd format. Default to today if not provided.");
+            Console.WriteLine("-r <root folder path>: Path to the root folder. Default to c:\\temp if not provided.");
+            Console.WriteLine("-a <archive path>    : Path to the archive folder, will overwrite root folder's archive path.");
+        }
+
+        static void Excute(string sourceFolder, string outputFolder, ILetterService letterService, string processDate, string archiveFolder)
+        {
+            string admissionFolder = Path.Combine(sourceFolder, "Admission", processDate);
+            string scholarshipFolder = Path.Combine(sourceFolder, "Scholarship", processDate);
+            var letterFiles = GetAllFilesToBeProcessed(new List<string> { admissionFolder, scholarshipFolder });
+            // Process admission letters
+            ProcessLetters(letterFiles, outputFolder, letterService, archiveFolder);
+        }
+
+        static List<string> GetAllFilesToBeProcessed(List<string> filePaths)
+        {
+            List<string> result = new List<string>();
+            foreach (string filePath in filePaths)
+            {
+                string[] files = Directory.GetFiles(filePath, "*.txt");
+                foreach (string file in files)
+                {
+                    result.Add(file);
+                }
+            }
+            return result;
+        }
+
+        static void ProcessLetters(List<string> filesToProcess, string outputFolder, ILetterService letterService, string archiveFolder)
+        {
+            var groupedLetters = GroupLettersByStudentId(filesToProcess);
+
+            foreach (string studentId in groupedLetters.Keys)
+            {
+                List<string> letterFilesForStudent = groupedLetters[studentId];
+                string letterPathForCombine = Path.Combine(outputFolder, $"combined-{studentId}.txt");
+                CombineMultipleLetters(letterFilesForStudent, letterPathForCombine, letterService);
+                ArchiveFiles(letterFilesForStudent, archiveFolder);
+            }
+        }
+
+        static void ArchiveFiles(List<string> filesToMove, string destination)
+        {
+            foreach (string file in filesToMove)
+            {
+                Directory.Move(file, Path.Combine(destination, Path.GetFileName(file)));
+            }
+        }
+
+        /*
+         * This will run through all files provided and group the file pathes by student id.
+         */
+        static Dictionary<string, List<string>> GroupLettersByStudentId(List<string> letterFiles)
+        {
+            var combinedLetters = new Dictionary<string, List<string>>();
+            foreach (string letterFile in letterFiles)
+            {
+                string universityID = Path.GetFileNameWithoutExtension(letterFile).Split('-')[1];
+
+                if (!combinedLetters.ContainsKey(universityID))
+                {
+                    combinedLetters[universityID] = new List<string>();
+                }
+
+                combinedLetters[universityID].Add(letterFile);
+            }
+            return combinedLetters;
         }
 
         static void CombineMultipleLetters(List<string> letterFiles, string combinedLetterPath, ILetterService letterService)
         {
-            // Sort the letter files to ensure consistent ordering
-            letterFiles.Sort();
-
             // Start with the first letter file
             string firstLetterFile = letterFiles[0];
-            File.Copy(firstLetterFile, combinedLetterPath);
+            if (!Path.Exists(combinedLetterPath)) { File.Copy(firstLetterFile, combinedLetterPath); }
 
             // Combine subsequent letter files
             for (int i = 1; i < letterFiles.Count; i++)
@@ -127,10 +186,6 @@ namespace ConsoleApp
                 string currentLetterFile = letterFiles[i];
                 letterService.CombineTwoLetters(combinedLetterPath, currentLetterFile, combinedLetterPath);
             }
-        
+        }
     }
-}
-
-    // ...
-    // Rest of the code
 }
